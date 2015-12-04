f0041e11f3f1edf7759e7c3743eb7dcc36946d33
Julie Pan <ycp217@nyu.edu>
2015-12-03 22:17:26 -0500

    day3 complete

--- day3/day3.go
+++ day3/day3.go
@@ -19,22 +19,39 @@ func main() {
 	count := 1
 	x := 0
 	y := 0
+	rx := 0
+	ry := 0
 	visited["0-0"] = true
 	key := ""
-	for _, c := range data {
+	for i, c := range data {
 		cha := string(c)
-		if cha == "^" {
-			y += 1
-		} else if cha == ">" {
-			x += 1
-		} else if cha == "v" {
-			y -= 1
-		} else if cha == "<" {
-			x -= 1
+		if i%2 == 0 {
+			if cha == "^" {
+				y += 1
+			} else if cha == ">" {
+				x += 1
+			} else if cha == "v" {
+				y -= 1
+			} else if cha == "<" {
+				x -= 1
+			} else {
+				break
+			}
+			key = strconv.Itoa(x) + "-" + strconv.Itoa(y)
 		} else {
-			break
+			if cha == "^" {
+				ry += 1
+			} else if cha == ">" {
+				rx += 1
+			} else if cha == "v" {
+				ry -= 1
+			} else if cha == "<" {
+				rx -= 1
+			} else {
+				break
+			}
+			key = strconv.Itoa(rx) + "-" + strconv.Itoa(ry)
 		}
-		key = strconv.Itoa(x) + "-" + strconv.Itoa(y)
 		if visited[key] {
 			// do nothing
 		} else {
